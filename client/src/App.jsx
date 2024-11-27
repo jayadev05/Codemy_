2ca3973811d4d6f3bbae7744ca52f900ad5fe941
Jayadev <jayadevnair05@gmail.com>
2024-11-27 23:46:04 +0530

    user profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,22 @@ import store from "../src/redux/store";
 import Dashboard from "./components/pages/admin/dashboard";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import TutorManagement from "./components/pages/admin/tutorManage";
+import UserProfile from "./components/pages/user/UserProfile";
+import SettingsPage from "./components/pages/user/UserSettings";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={
-            <GoogleOAuthProvider clientId="532055856231-5bvv6o4cog4srbvvghv969kfenmd33cl.apps.googleusercontent.com">
+          <Route
+            path="/signup"
+            element={
+              <GoogleOAuthProvider clientId="532055856231-5bvv6o4cog4srbvvghv969kfenmd33cl.apps.googleusercontent.com">
                 <Signup />
-            </GoogleOAuthProvider>
-          
-           
-            } />
+              </GoogleOAuthProvider>
+            }
+          />
           <Route
             path="/login"
             element={
@@ -32,7 +35,13 @@ function App() {
           />
           <Route path="/" element={<Home />} />
           <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/manage-tutor" element={<TutorManagement/>} />
+          <Route path="/admin/manage-tutor" element={<TutorManagement />} />
+
+
+          <Route path="/user/profile" element={<UserProfile/>} />
+          <Route path="/user/settings" element={<SettingsPage/>} />
+
+
         </Routes>
       </BrowserRouter>
     </Provider>
